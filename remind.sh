eada220a6241e17621f1500b6e36af53d4653b6a
caithesi <caithesi@gmail.com>
2025-09-01 16:04:06 +0700

    override the first call when call remind twice

--- remind.sh
+++ remind.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Usage: ./remind.sh 5 "Take a break"
-# -> After 5 minutes, will show a notification "Take a break"
+# If you run it again, it cancels the previous reminder and sets the new one.
+
+PIDFILE="$HOME/.remind_pid"
 
 if [ $# -lt 2 ]; then
   echo "Usage: $0 <minutes> <message>"
@@ -12,5 +14,19 @@ minutes=$1
 shift
 message="$*"
 
-# Sleep for given minutes in background
-(sleep $((minutes * 60)) && notify-send "Reminder" "$message") &
+# Kill previous reminder if running
+if [ -f "$PIDFILE" ]; then
+  oldpid=$(cat "$PIDFILE")
+  if kill -0 "$oldpid" 2>/dev/null; then
+    kill "$oldpid"
+  fi
+fi
+
+# Start new reminder
+(
+  sleep $((minutes * 60))
+  notify-send "Reminder" "$message"
+) &
+
+# Save its PID
+echo $! > "$PIDFILE"
